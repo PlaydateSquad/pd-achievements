f5859be6f0dda9774cbe1da456e1406a73b21bb4
DimitriBarronmore <d_barronmore@outlook.com>
2024-11-04 13:05:19 -0700

    fix a funny oversight making the default toast silently error

--- achievements/graphics.lua
+++ achievements/graphics.lua
@@ -111,8 +111,8 @@ local function draw_card_unsafe(ach, x, y, msec_since_granted)
 				if ach.granted_at then
 					select_icon = ach.icon or achievements.gameData.defaultIcon or "*_default_icon"
 				end
-				get_image(select_icon):draw(4, 4)
-				get_image(select_icon):draw(324, 4)
+				toast_graphics.get_image(select_icon):draw(4, 4)
+				toast_graphics.get_image(select_icon):draw(324, 4)
 				gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
 				-- TODO: get our own font in here, so we don't use the font users have set outside of the lib
 				gfx.drawTextInRect(ach.name, 40, 14, 292, 60, nil, "...", kTextAlignment.center)
