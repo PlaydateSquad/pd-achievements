3419c118e1c4f33330d3b78f3f7be68de05b212f
Jon Simantov <jsimantov@gmail.com>
2025-03-03 12:24:29 -0800

    Remove out of date comment.

--- embed_images.sh
+++ embed_images.sh
@@ -3,7 +3,6 @@
 # TODO: build equivalent in powershell (and/or cmd-term, sigh) for windows (current setup works fo linux and _should_ work for macs)
 
 echo "NOTE: Running this script should only be necesary whenever the base default _fallback_ icons are updated by maintainers of the library."
-echo "      Setting custom default icons for your game can be done by simply adding 'defaultIcon' and 'defaultIconLocked' fields in your initialization-data."
 
 python  ./maintainer_resources/embed_images.py \
 	--image-file  ./maintainer_resources/achievement-unlock.png    '["*_b64_default_icon"]'    \
