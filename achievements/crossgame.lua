417a9ebaf251c056bd6e0c5afc097761e5346d60
gurtt <6712553+gurtt@users.noreply.github.com>
2025-04-27 21:25:21 +1000

    Add boilerplate and doc comments for crossgame

--- achievements/crossgame.lua
+++ achievements/crossgame.lua
@@ -1,4 +1,13 @@
--- Cross-game data reading module for the PlaydateSquad Achievements library.
+--[[
+	PlaydateSquad Achievements Library
+	https://github.com/PlaydateSquad/pd-achievements
+
+	This library provides an implementation of a shared format for achievements on Playdate.
+	See the README.md file for more information.
+
+	This software is released to the public domain using the Unlicense license agreement <https://unlicense.org>.
+--]]
+
 if not (achievements and achievements.flag_is_playdatesquad_api) then
 	error("Achievements 'crossgame' module must be loaded after the base PlaydateSquad achievement library.")
 end
@@ -7,11 +16,17 @@ local crossgame = {}
 local gfx <const> = playdate.graphics
 achievements.crossgame = crossgame
 
--- Returns whether a game has any listed achievement data.
+--- Checks if a shared data folder exists for a game with the supplied `game_id`.
+--- 
+--- @param game_id string The ID of the game to check.
+--- @return boolean # Whether a shared data folder exists for the game.
 crossgame.gamePlayed = function(game_id)
 	return playdate.file.isdir(achievements.paths.get_achievement_folder_root_path(game_id))
 end
 
+--- Returns a list of game IDs with shared data folders.
+--- 
+--- @return table # A list of game IDs with shared data folders.
 crossgame.listGames = function()
 	local games = {}
 	for _, path in ipairs(playdate.file.listFiles(achievements.paths.shared_data_root)) do
@@ -23,9 +38,12 @@ crossgame.listGames = function()
 	return games
 end
 
----@param game_id string
----@return game_data
--- Returns the achievement data for the requested game if it exists. Otherwise returns false and a reason.
+--- Deserializes the shared achievement data for the game with the supplied `game_id`.
+--- 
+--- This function doesn't validate that the data is valid.
+--- 
+--- @param game_id string The ID of the game for which to get data.
+--- @return game_data # The achievement data for the game, or false if it doesn't exist.
 crossgame.getData = function(game_id)
 	if not crossgame.gamePlayed then
 		return false, "No achievement data for game '" .. game_id .. "' was found."
@@ -55,6 +73,11 @@ crossgame.getData = function(game_id)
 	return data
 end
 
+--- Loads an image from the shared images folder for the game with the supplied `game_id`.
+--- 
+--- @param game_id string The ID of the game for which to load the image.
+--- @param filepath string The path to the image file, relative to the shared images folder.
+--- @return playdate.graphics.image # The loaded image, or nil if it couldn't be loaded.
 function crossgame.loadImage(game_id, filepath)
 	local image_path = achievements.paths.get_shared_images_path(game_id) .. filepath
 	local img, err = gfx.image.new(image_path)
